1900b101c4c8640b864dfa0dff82661e069cfacd
nwill7 <88152709+nwill7@users.noreply.github.com>
2024-04-07 16:31:31 -0700

    Update SuperCoolScript.js
    Added a bunch of math functions.
    Random does not work yet

--- js/SuperCoolScript.js
+++ js/SuperCoolScript.js
@@ -5,8 +5,15 @@ var button_clicked = function(){
     var res;
     var ans = "There are 6.84 billion smartphones globally. It is unlikely that you do not own one. Why wouldn't you use that instead of a website to add numbers together?";
 
-    res = num1 + num2;
-    console.log(res);
+    console.log(num1,' + ', num2, ' = ', num1 + num2);
+    console.log(num1, ' - ', num2, ' = ', num1 - num2);
+    console.log(num1, ' * ', num2, ' = ', num1 * num2);
+    console.log(num1, ' / ', num2, ' = ', num1 / num2);
+    console.log(num1, ' ^ ', num2, ' = ', Math.pow(num1, num2));
+    console.log(num1, ' % ', num2, ' = ', num1 % num2);
+    console.log('Minimum of ', num1, ' and ', num2, ' is ', Math.min(num1, num2));
+    console.log('Maximum of ', num1, ' and ', num2, ' is ', Math.max(num1, num2));
+    console.log('Random number between ', num1, ' and ', num2, ' is ', Math.floor((Math.random() * (Math.max(num1, num2))) + (Math.min(num1, num2))));
 
     document.getElementById("answer").innerHTML = ans;
 };
